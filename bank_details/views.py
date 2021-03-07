71ccd2030b6e14180f057be6a846e31e1f04c919
Abiraj <abhi44raj@gmail.com>
2021-03-07 19:17:56 +0530

    added changes

--- bank_details/views.py
+++ bank_details/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Branches
 from django.core.paginator import Paginator
 from django.db.models import Q
+import requests
 from django.http import HttpResponse
 import json
 
