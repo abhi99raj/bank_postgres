8eeadc9e77e2afc44f29f8f633ff506a2e7a2117
Abiraj <abhi44raj@gmail.com>
2021-03-07 20:29:45 +0530

    Added data

--- Bank/settings.py
+++ Bank/settings.py
@@ -87,8 +87,17 @@ WSGI_APPLICATION = 'Bank.wsgi.application'
 # }
 
 # DEBUG = config('DEBUG', default=False, cast=bool)
+
+
+#import dj_database_url
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600, ssl_require=True
+    )
+}
 
 # DATABASES = {
 #     'default': {
