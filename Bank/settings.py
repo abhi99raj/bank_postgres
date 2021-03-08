34eb7ca779354d4be1225c25ba5e4de9c1693779
Abiraj <abhi44raj@gmail.com>
2021-03-08 20:00:15 +0530

    make it better

--- Bank/settings.py
+++ Bank/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Bank.wsgi.application'
 
 
 Database
-https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+#https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
